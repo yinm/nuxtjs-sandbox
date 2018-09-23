f9d8aee921c016326aaccd7eef2fcd357bc65239
yinm <yinm.cminor9@gmail.com>
2018-09-23 13:01:38 +0900

    Add TS module
    ref: https://github.com/nuxt-community/typescript-template/issues/48#issuecomment-396437560

--- nuxt2-and-typescript/nuxt.config.js
+++ nuxt2-and-typescript/nuxt.config.js
@@ -40,7 +40,8 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '~/modules/typescript.js'
   ],
   /*
   ** Axios module configuration
