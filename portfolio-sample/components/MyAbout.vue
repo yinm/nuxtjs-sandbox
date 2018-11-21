c20edc4b2f2bff783eedebbaf035d10165949e5e
yinm <yinm.cminor9@gmail.com>
2018-11-21 20:31:23 +0900

    Update About

---
+++ portfolio-sample/components/MyAbout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1 class="title">About</h1>
+
+    <div class="text">
+      <p class="name">yinm</p>
+      <p class="description">I'm a Web Application Engineer.</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class MyAbout extends Vue {}
+</script>
+
+<style scoped>
+.title {
+  font-size: 2rem;
+}
+
+.text {
+  margin-top: 40px;
+}
+
+.text p {
+  margin-top: 10px;
+}
+
+.name {
+  font-weight: bold;
+}
+</style>
